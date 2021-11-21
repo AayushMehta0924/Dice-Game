e21b58785e19cf0f18ee2e4d0259f8a3199bf813
Aayush Mehta <aayushmehta0924@gmail.com>
2021-11-21 15:41:38 +0530

    Got the dice randomized

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// Left image
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage1 = "dice" + randomNumber1 + ".png";
+
+var randomSource1 = "images/" + randomImage1;
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomSource1);
+
+
+// Right image
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage2 = "dice" + randomNumber2 + ".png";
+
+var randomSource2 = "images/" + randomImage2;
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomSource2);
+
+
+
+
+
