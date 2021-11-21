872288eae4755fabee5e707181f0d2baeee685dc
Aayush Mehta <aayushmehta0924@gmail.com>
2021-11-21 18:28:11 +0530

    Created code to decide the result of the game

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ var randomSource2 = "images/" + randomImage2;
 var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomSource2);
 
 
-
-
+// Decider
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
 
